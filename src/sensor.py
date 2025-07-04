72aab993b4877d1a4d243fce41117850dfcac0c3
tanelr94 <20149102@tafe.wa.edu.au>
2025-07-04 13:20:57 +0300

    feat: add detect_vehicle and update_car_park methods to Sensor class

--- src/sensor.py
+++ src/sensor.py
@@ -1,4 +1,5 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+import random
 
 class Sensor(ABC):
     def __init__(self, id, is_active, car_park):
@@ -9,8 +10,27 @@ class Sensor(ABC):
     def __str__(self):
         return f"Sensor {self.id}: {'Active' if self.is_active else 'Inactive'}"
 
+    def _scan_plate(self):
+        return 'FAKE-' + format(random.randint(0, 999), "03d")
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
+
+    @abstractmethod
+    def update_car_park(self, plate):
+        pass
+
 class EntrySensor(Sensor):
-    pass
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
 
 class ExitSensor(Sensor):
-    pass
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates) if self.car_park.plates else 'FAKE-000'
+
+    def update_car_park(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
+
