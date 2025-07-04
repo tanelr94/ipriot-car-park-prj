025f3202ca30462fa32a30c9843f01e60b69f9c4
tanelr94 <20149102@tafe.wa.edu.au>
2025-07-04 13:09:53 +0300

    feat: add constructors and attributes to Sensor and subclasses

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,16 @@
-class Sensor:
+from abc import ABC
+
+class Sensor(ABC):
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"Sensor {self.id}: {'Active' if self.is_active else 'Inactive'}"
+
+class EntrySensor(Sensor):
+    pass
+
+class ExitSensor(Sensor):
     pass
